04c6663b58adf04ff8ebcf89a2dfa3a6711a1223
Gabriel Luong <gabriel.luong@gmail.com>
2020-08-23 18:05:47 -0400

    WIP

--- app/src/main/java/org/mozilla/fenix/home/sessioncontrol/SessionControlView.kt
+++ app/src/main/java/org/mozilla/fenix/home/sessioncontrol/SessionControlView.kt
@@ -32,7 +32,7 @@ private fun normalModeAdapterItems(
     tip?.let { items.add(AdapterItem.TipItem(it)) }
 
     if (topSites.isNotEmpty()) {
-        items.add(AdapterItem.TopSiteList(topSites))
+        items.add(AdapterItem.TopSitePager(topSites))
     }
 
     if (collections.isEmpty()) {
@@ -141,7 +141,7 @@ class SessionControlView(
             sessionControlAdapter.submitList(stateAdapterList) {
 
                 val loadedTopSites = stateAdapterList.find { adapterItem ->
-                    adapterItem is AdapterItem.TopSiteList && adapterItem.topSites.isNotEmpty()
+                    adapterItem is AdapterItem.TopSitePager && adapterItem.topSites.isNotEmpty()
                 }
                 loadedTopSites?.run {
                     homeScreenViewModel.shouldScrollToTopSites = false
