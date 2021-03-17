c730603f93ed028f1034580c539456bddb568f8e
Gabriel Luong <gabriel.luong@gmail.com>
2021-03-17 19:36:28 -0400

    For #18246 - [Add card] Populate the Credit card expiry month drop down

--- app/src/main/java/org/mozilla/fenix/settings/creditcards/CreditCardEditorFragment.kt
+++ app/src/main/java/org/mozilla/fenix/settings/creditcards/CreditCardEditorFragment.kt
@@ -6,11 +6,15 @@ package org.mozilla.fenix.settings.creditcards
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_credit_card_editor.view.*
 import org.mozilla.fenix.R
 import org.mozilla.fenix.ext.showToolbar
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 /**
  * Display a credit card editor for adding and editing a credit card.
@@ -25,5 +29,27 @@ class CreditCardEditorFragment : Fragment(R.layout.fragment_credit_card_editor) 
         view.cancel_button.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        setupExpiryMonthDropDown(view)
+    }
+
+    /**
+     * Setup the expiry month dropdown by formatting and populating it with the months in a calendar
+     * year.
+     */
+    private fun setupExpiryMonthDropDown(view: View) {
+        val adapter =
+            ArrayAdapter<String>(view.context, android.R.layout.simple_spinner_dropdown_item)
+        val dateFormat = SimpleDateFormat("MMMM (MM)", Locale.getDefault())
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+
+        for (month in 0..11) {
+            calendar.set(Calendar.MONTH, month)
+            adapter.add(dateFormat.format(calendar.time))
+        }
+
+        view.expiry_month_drop_down.adapter = adapter
     }
 }
